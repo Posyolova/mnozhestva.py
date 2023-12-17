d17913476486b7fbbd2384695126fb74fec3c29d
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-12-17 19:02:10 +0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,8 +1,9 @@
-a = open('input.txt',)
-n = set()
-for q in a:
-    w = q.split()
-    for i in w:
-        n.add(i.rstrip('\n'))
-a.close()
-print(len(n))
+with open('input.txt', 'r', encoding='utf-8') as file:
+    text = file.read()  
+
+words = text.split()  # Разбиваем текст на слова )
+
+unique_words = set(words)  # определениe уникальных слов
+count_unique_words = len(unique_words)  # Считаем количество уникальных слов
+
+print("Количество различных слов в тексте:", count_unique_words)
